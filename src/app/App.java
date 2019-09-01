8840e08e4c87d086b4d4e758f8bb8394decb7ca6
Jeff Wong <jeffmjwong@gmail.com>
2019-09-01 17:52:31 +1000

    Create a for loop

--- src/app/App.java
+++ src/app/App.java
@@ -7,19 +7,23 @@ public class App {
     double result;
     char opCode = 'd';
 
-    if (opCode == 'a') {
-      result = val1 + val2;
-    } else if (opCode == 's') {
-      result = val1 - val2;
-    } else if (opCode == 'm') {
-      result = val1 * val2;
-    } else if (opCode == 'd') {
-      result = val2 == 0.0 ? 0.0 : val1 / val2;
-    } else {
-      System.out.println("Error - invalid opCode!");
-      result = 0.0;
+    for (double i = val1; i > 1; i -= 30) {
+      System.out.println(i);
     }
 
-    System.out.println(result);
+    // if (opCode == 'a') {
+    //   result = val1 + val2;
+    // } else if (opCode == 's') {
+    //   result = val1 - val2;
+    // } else if (opCode == 'm') {
+    //   result = val1 * val2;
+    // } else if (opCode == 'd') {
+    //   result = val2 == 0.0 ? 0.0 : val1 / val2;
+    // } else {
+    //   System.out.println("Error - invalid opCode!");
+    //   result = 0.0;
+    // }
+
+    // System.out.println(result);
   }
 }
