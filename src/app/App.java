c47710dd2dedd88ad9cbcc7ea0ecad647df686b8
Jeff Wong <jeffmjwong@gmail.com>
2019-09-01 19:29:29 +1000

    Create an array

--- src/app/App.java
+++ src/app/App.java
@@ -3,27 +3,28 @@ package app;
 public class App {
   public static void main(String[] args) throws Exception {
     double val1 = 100.0;
-    double val2 = 0.0;
+    double val2 = 50.0;
     double result;
     char opCode = 'd';
 
-    for (double i = val1; i > 1; i -= 30) {
+    double[] arr1 = {val1, val2};
+    for (double i : arr1) {
       System.out.println(i);
     }
 
-    // if (opCode == 'a') {
-    //   result = val1 + val2;
-    // } else if (opCode == 's') {
-    //   result = val1 - val2;
-    // } else if (opCode == 'm') {
-    //   result = val1 * val2;
-    // } else if (opCode == 'd') {
-    //   result = val2 == 0.0 ? 0.0 : val1 / val2;
-    // } else {
-    //   System.out.println("Error - invalid opCode!");
-    //   result = 0.0;
-    // }
+    if (opCode == 'a') {
+      result = val1 + val2;
+    } else if (opCode == 's') {
+      result = val1 - val2;
+    } else if (opCode == 'm') {
+      result = val1 * val2;
+    } else if (opCode == 'd') {
+      result = val2 == 0.0 ? 0.0 : val1 / val2;
+    } else {
+      System.out.println("Error - invalid opCode!");
+      result = 0.0;
+    }
 
-    // System.out.println(result);
+    System.out.println(result);
   }
 }
