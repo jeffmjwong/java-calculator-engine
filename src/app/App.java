d630cc238d1254eccabbe6c990d7f17052c1e7dd
Jeff Wong <jeffmjwong@gmail.com>
2019-09-01 16:09:17 +1000

    Use .equals() instead of == to compare strings

--- src/app/App.java
+++ src/app/App.java
@@ -1,7 +1,17 @@
 package app;
 
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello Java");
-    }
+  public static void main(String[] args) throws Exception {
+    String str1 = "haha";
+    String str2 = "haha";
+
+    System.out.println(str1.equals(str2));
+
+    // double val1;
+    // double val2;
+    // double result;
+    // char opCode;
+
+    // if
+  }
 }
