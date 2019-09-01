4348961dbc56117c78970dd353f3f544337cfff0
Jeff Wong <jeffmjwong@gmail.com>
2019-09-01 17:19:02 +1000

    Check for divisor not to be 0.0

--- src/app/App.java
+++ src/app/App.java
@@ -3,9 +3,9 @@ package app;
 public class App {
   public static void main(String[] args) throws Exception {
     double val1 = 100.0;
-    double val2 = 50.0;
+    double val2 = 0.0;
     double result;
-    char opCode = 'e';
+    char opCode = 'd';
 
     if (opCode == 'a') {
       result = val1 + val2;
@@ -14,7 +14,7 @@ public class App {
     } else if (opCode == 'm') {
       result = val1 * val2;
     } else if (opCode == 'd') {
-      result = val1 / val2;
+      result = val2 == 0.0 ? 0.0 : val1 / val2;
     } else {
       System.out.println("Error - invalid opCode!");
       result = 0.0;
