81b1996c463f223beb23bce1024765dac38d5784
Jeff Wong <jeffmjwong@gmail.com>
2019-09-01 17:10:34 +1000

    Print error message if invalid opCode

--- src/app/App.java
+++ src/app/App.java
@@ -5,7 +5,7 @@ public class App {
     double val1 = 100.0;
     double val2 = 50.0;
     double result;
-    char opCode = 'a';
+    char opCode = 'e';
 
     if (opCode == 'a') {
       result = val1 + val2;
@@ -16,6 +16,7 @@ public class App {
     } else if (opCode == 'd') {
       result = val1 / val2;
     } else {
+      System.out.println("Error - invalid opCode!");
       result = 0.0;
     }
 
