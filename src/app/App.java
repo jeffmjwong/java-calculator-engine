81f272b1e9d72e8fe32d83c15ec8e5de95d936bc
Jeff Wong <jeffmjwong@gmail.com>
2019-09-01 19:35:12 +1000

    Use for loop to loop through an array

--- src/app/App.java
+++ src/app/App.java
@@ -7,24 +7,26 @@ public class App {
     double result;
     char opCode = 'd';
 
-    double[] arr1 = {val1, val2};
-    for (double i : arr1) {
-      System.out.println(i);
+    double[] arr1 = {2.5, 3.5, 5.5};
+    double sum = 0.0;
+    for (int i = 0; i < arr1.length; i++) {
+      sum += arr1[i];
     }
+    System.out.println(sum);
 
-    if (opCode == 'a') {
-      result = val1 + val2;
-    } else if (opCode == 's') {
-      result = val1 - val2;
-    } else if (opCode == 'm') {
-      result = val1 * val2;
-    } else if (opCode == 'd') {
-      result = val2 == 0.0 ? 0.0 : val1 / val2;
-    } else {
-      System.out.println("Error - invalid opCode!");
-      result = 0.0;
-    }
+    // if (opCode == 'a') {
+    //   result = val1 + val2;
+    // } else if (opCode == 's') {
+    //   result = val1 - val2;
+    // } else if (opCode == 'm') {
+    //   result = val1 * val2;
+    // } else if (opCode == 'd') {
+    //   result = val2 == 0.0 ? 0.0 : val1 / val2;
+    // } else {
+    //   System.out.println("Error - invalid opCode!");
+    //   result = 0.0;
+    // }
 
-    System.out.println(result);
+    // System.out.println(result);
   }
 }
