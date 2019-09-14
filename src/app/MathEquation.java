8f8356fa5c01e64725e75e8a787b92e00dd176c2
Jeff Wong <jeffmjwong@gmail.com>
2019-09-14 21:53:39 +1000

    Create getters and setters in MathEquation class

--- src/app/MathEquation.java
+++ src/app/MathEquation.java
@@ -6,6 +6,17 @@ public class MathEquation {
   private char opCode;
   private double result;
 
+  public double getLeftVal() { return leftVal; }
+  public void setLeftVal(double leftVal) { this.leftVal = leftVal; }
+
+  public double getRightVal() { return rightVal; }
+  public void setRightVal(double rightVal) { this.rightVal = rightVal; }
+
+  public char getOpCpde() { return opCode; }
+  public void setOpCode(char opCode) { this.opCode = opCode; }
+
+  public double getResult() { return result; }
+
   public void execute() {
     switch (opCode) {
       case 'a':
