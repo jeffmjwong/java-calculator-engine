01062c0334db132f5bdea6ba1acbac4f600ea6ef
Jeff Wong <jeffmjwong@gmail.com>
2019-09-14 21:41:55 +1000

    Remove redundant code

--- src/app/App.java
+++ src/app/App.java
@@ -2,11 +2,6 @@ package app;
 
 public class App {
   public static void main(String[] args) throws Exception {
-    // double[] leftVals = { 100.0, 25.0, 225.0, 11.0, 16.0 };
-    // double[] rightVals = { 50.0, 92.0, 17.0, 3.0, 9.0 };
-    // char[] opCodes = { 'd', 'a', 's', 'm', 'l' };
-    // double[] results = new double[opCodes.length];
-
     MathEquation[] equations = new MathEquation[4];
     equations[0] = create(100.0d, 50.0d, 'd');
     equations[1] = create(25.0d, 92.0d, 'a');
