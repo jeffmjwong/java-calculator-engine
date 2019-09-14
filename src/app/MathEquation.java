481768df4897ab4049e780693c52e08cab2375c7
Jeff Wong <jeffmjwong@gmail.com>
2019-09-14 21:41:43 +1000

    Modify MathEquation class

--- src/app/MathEquation.java
+++ src/app/MathEquation.java
@@ -3,4 +3,29 @@ package app;
 public class MathEquation {
   public double leftVal;
   public double rightVal;
+  public char opCode;
+  public double result;
+
+  public void execute() {
+    switch (opCode) {
+      case 'a':
+        result = leftVal + rightVal;
+        break;
+      case 's':
+        result = leftVal - rightVal;
+        break;
+      case 'm':
+        result = leftVal * rightVal;
+        break;
+      case 'd':
+        result = rightVal == 0.0 ? 0.0 : leftVal / rightVal;
+        break;
+      case 'l':
+        result = leftVal % rightVal;
+        break;
+      default:
+        System.out.println("Error - invalid opCode!");
+        result = 0.0;
+    }
+  }
 }
