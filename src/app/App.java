02650ad7252e23180b9d09ac7b016ef4653b441d
Jeff Wong <jeffmjwong@gmail.com>
2019-09-15 19:29:42 +1000

    Use constructors instead of setting values

--- src/app/App.java
+++ src/app/App.java
@@ -2,23 +2,18 @@ package app;
 
 public class App {
   public static void main(String[] args) throws Exception {
-    MathEquation testEquation = new MathEquation();
-    testEquation.execute();
-    System.out.print("result = ");
-    System.out.println(testEquation.getResult());
+    MathEquation[] equations = new MathEquation[4];
+    equations[0] = create(100.0d, 50.0d, 'd');
+    equations[1] = create(25.0d, 92.0d, 'a');
+    equations[2] = create(225.0d, 17.0d, 's');
+    equations[3] = create(11.0d, 3.0d, 'm');
 
-    // MathEquation[] equations = new MathEquation[4];
-    // equations[0] = create(100.0d, 50.0d, 'd');
-    // equations[1] = create(25.0d, 92.0d, 'a');
-    // equations[2] = create(225.0d, 17.0d, 's');
-    // equations[3] = create(11.0d, 3.0d, 'm');
+    for (MathEquation equation : equations) {
+      equation.execute();
 
-    // for (MathEquation equation : equations) {
-    //   equation.execute();
-
-    //   System.out.print("result = ");
-    //   System.out.println(equation.getResult());
-    // }
+      System.out.print("result = ");
+      System.out.println(equation.getResult());
+    }
   }
 
   public static MathEquation create(double leftVal, double rightVal, char opCode) {
