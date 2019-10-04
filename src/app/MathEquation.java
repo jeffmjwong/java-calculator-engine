03e9a2b0e6ff1f4d7eb893bb7cc8b6a90250f5e0
Jeff Wong <jeffmjwong@gmail.com>
2019-10-04 19:35:12 +1000

    Create execute() overload method

--- src/app/MathEquation.java
+++ src/app/MathEquation.java
@@ -29,6 +29,13 @@ public class MathEquation {
     this.rightVal = rightVal;
   }
 
+  public void execute(double leftVal, double rightVal) {
+    this.leftVal = leftVal;
+    this.rightVal = rightVal;
+
+    execute();
+  }
+
   public void execute() {
     switch (opCode) {
       case 'a':
