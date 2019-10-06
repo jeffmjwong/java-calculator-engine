51b1199341f25110a873f7a1fec5eda9a965e439
Jeff Wong <jeffmjwong@gmail.com>
2019-10-06 11:35:43 +1000

    Create methods in Pilot class

--- src/app/Pilot.java
+++ src/app/Pilot.java
@@ -6,4 +6,20 @@ public class Pilot {
   public Flight getCurrentFlight() {
     return this.currentFlight;
   }
+
+  public void fly(Flight flight) {
+    if (canAccept(flight)) {
+      this.currentFlight = flight;
+    } else {
+      handleCantAccept();
+    }
+  }
+
+  private boolean canAccept(Flight flight) {
+    return true;
+  }
+
+  private void handleCantAccept() {
+    System.out.println("Can't accept the flight!");
+  }
 }
