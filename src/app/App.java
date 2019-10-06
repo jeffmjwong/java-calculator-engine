78c0d91e2df7ea1a13bac98d5d579cbdcdc0fc5b
Jeff Wong <jeffmjwong@gmail.com>
2019-10-06 15:33:31 +1000

    Update App.java file

--- src/app/App.java
+++ src/app/App.java
@@ -4,9 +4,11 @@ public class App {
   public static void main(String[] args) throws Exception {
     Flight f1 = new Flight(175);
     Flight f2 = new Flight(175);
+    CargoFlight f3 = new CargoFlight(500, 200.0f);
     // Passenger p1 = new Passenger();
 
     System.out.println(f1.equals(f2));
+    System.out.println(f1.equals(f3));
 
     // System.out.println(f1.getPassengers());
     // f1.add1Passenger();
