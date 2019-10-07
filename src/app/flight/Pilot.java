82de013f488b1dce0e557ceec85dcb83a2c7ff76
Jeff Wong <jeffmjwong@gmail.com>
2019-10-07 14:42:26 +1000

    Move Pilot.java file

---
+++ src/app/flight/Pilot.java
@@ -0,0 +1,23 @@
+package app.flight;
+
+public abstract class Pilot {
+  private Flight currentFlight;
+
+  public Flight getCurrentFlight() {
+    return this.currentFlight;
+  }
+
+  public void fly(Flight flight) {
+    if (canAccept(flight)) {
+      this.currentFlight = flight;
+    } else {
+      handleCantAccept();
+    }
+  }
+
+  public abstract boolean canAccept(Flight flight);
+
+  private void handleCantAccept() {
+    System.out.println("Can't accept the flight!");
+  }
+}
