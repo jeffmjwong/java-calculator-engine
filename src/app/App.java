181edd4d60a8d894eb980fc4a0ff80e0430c9d85
Jeff Wong <jeffmjwong@gmail.com>
2019-10-07 15:40:01 +1000

    Update App.java file

--- src/app/App.java
+++ src/app/App.java
@@ -5,17 +5,18 @@ import app.flight.CargoFlight;
 
 public class App {
   public static void main(String[] args) throws Exception {
-    Flight f1 = new Flight(175);
-    Flight f2 = new Flight(175);
-    CargoFlight f3 = new CargoFlight(500, 200.0f);
+    // Flight f1 = new Flight(175);
+    // Flight f2 = new Flight(175);
+    // CargoFlight f3 = new CargoFlight(500, 200.0f);
     // Passenger p1 = new Passenger();
 
-    System.out.println(f1.equals(f2));
-    System.out.println(f1.equals(f3));
+    // System.out.println(f1.equals(f2));
+    // System.out.println(f1.equals(f3));
 
-    // System.out.println(f1.getPassengers());
-    // f1.add1Passenger();
-    // System.out.println(f1.getPassengers());
+    Adder a1 = new Adder(5.0, 6.0);
+    a1.calculate();
+    System.out.println(a1.getResult());
+
     // MathEquation[] equations = new MathEquation[4];
     // equations[0] = create(100.0d, 50.0d, 'd');
     // equations[1] = create(25.0d, 92.0d, 'a');
