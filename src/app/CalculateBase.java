6e52860b1a6670b71c38bc681a2de65402933870
Jeff Wong <jeffmjwong@gmail.com>
2019-10-07 15:16:39 +1000

    Set up CalculateBase as abstract class

--- src/app/CalculateBase.java
+++ src/app/CalculateBase.java
@@ -1,7 +1,23 @@
 package app;
 
-public class CalculateBase {
+public abstract class CalculateBase {
   private double leftVal;
   private double rightVal;
   private double result;
+
+  public double getLeftVal() { return this.leftVal; }
+  public void setLeftVal(double leftVal) { this.leftVal = leftVal; }
+  public double getRightVal() { return this.rightVal; }
+  public void setRightVal(double rightVal) { this.rightVal = rightVal; }
+  public double getResult() { return this.result; }
+  public void setResult(double result) { this.result = result; }
+
+  public CalculateBase() {}
+
+  public CalculateBase(double leftVal, double rightVal) {
+    this.setLeftVal(leftVal);
+    this.setRightVal(rightVal);
+  }
+
+  public abstract void calculate();
 }
